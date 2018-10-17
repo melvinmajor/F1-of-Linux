8aa1a56c581f3ec244b5fca31a782ae786a364c8
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2018-10-17 12:06:29 +0200

    Clean projet.c

--- projet.c
+++ projet.c
@@ -1,44 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-int f1[20] = {44, 77, 5,7,3,33,11,31,18,35,27,55,10,28,8,20,2,14,9,16};
+int f1[20] = {44, 77, 5,  7,  3, 33, 11, 31, 18, 35,
+              27, 55, 10, 28, 8, 20, 2,  14, 9,  16};
 
+int main(int argc, char *argv[]) {
+  int min = 30 * 60 * 1000;
+  int max = 45 * 60 * 1000;
 
-int main(int argc, char *argv[]){
-    printf("%d" rand());
-    my_rand();
-    return 0;	
+  for (int i = 0; i < 20; i++) {
+    printf("name: %d, time:%d\n", f1[i], my_rand(min, max));
+  }
+
+  exit(0);
 }
 
-int my_rand(){
-    int min = 1800000;
-    int max = 2700000;
-    int m, s, ms;
-    int i;
-    int temps;
-    static int first = 0;
-
-    char tempsConv[10];
-    char tempsConcat[10];
-
-    if(first==0){
-        srand(time(NULL));
-        first = 1;
-    }
-
-    for(i = 0; i<3; i++){
-        int random = rand()%(max+1-min)+min;
-        s = random/1000;
-        ms = random%1000;
-        m = s/60;
-        s= s%60;
-
-        sprintf(tempsConv, "%d %d %d", m,s,ms);
-        sprintf(tempsConcat, "%s", tempsConv);
-
-        printf("%s \n",tempsConcat);
-    }
-}
-
-
+int my_rand(int min, int max) { return rand() % (max + 1 - min) + min; }
