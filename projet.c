668ce7cdad76dec12b902ee2c514ad91ba3f3c9a
Hubert Van De Walle <h.vandewalle@students.ephec.be>
2018-10-17 15:47:55 +0200

    Add TimeUnit
    Removed tests

--- projet.c
+++ projet.c
@@ -1,15 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int f1[20] = {44, 77, 5,  7,  3, 33, 11, 31, 18, 35,
-              27, 55, 10, 28, 8, 20, 2,  14, 9,  16};
+#define TABLE_SIZE 20
+
+int f1[TABLE_SIZE] = {44, 77, 5,  7,  3, 33, 11, 31, 18, 35,
+                      27, 55, 10, 28, 8, 20, 2,  14, 9,  16};
+
+struct TimeUnit {
+  int minutes;
+  int seconds;
+  int milliseconds;
+};
+
+struct TimeUnit toTimeUnit(int ms) {
+  struct TimeUnit timeUnit;
+  timeUnit.seconds = ms / 1000;
+  timeUnit.milliseconds = ms % 1000;
+  timeUnit.minutes = timeUnit.seconds / 60;
+  timeUnit.seconds = timeUnit.seconds % 60;
+  return timeUnit;
+}
+
+int toMs(struct TimeUnit *timeUnit) {
+  int ms;
+  ms += timeUnit->milliseconds;
+  ms += timeUnit->seconds * 60;
+  ms += timeUnit->minutes * 60 * 1000;
+  return ms;
+}
 
 int main(int argc, char *argv[]) {
-  int min = 30 * 60 * 1000;
-  int max = 45 * 60 * 1000;
+  struct TimeUnit min;
+  min.minutes = 0;
+  min.seconds = 30;
+  min.milliseconds = 0;
 
-  for (int i = 0; i < 20; i++) {
-    printf("name: %d, time:%d\n", f1[i], my_rand(min, max));
+  struct TimeUnit max;
+  max.minutes = 0;
+  max.seconds = 45;
+  max.milliseconds = 0;
+
+  int times[TABLE_SIZE];
+
+  for (int i = 0; i < TABLE_SIZE; i++) {
+    printf("name: %d, time: %d\n", f1[i], my_rand(toMs(&min), toMs(&max)));
   }
 
   exit(0);
